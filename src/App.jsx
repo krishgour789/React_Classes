3da33193c94cace11776256952fc8a50b894fcf3
krishgour789 <krishgour789@gmail.com>
2025-11-18 16:35:51 +0530

    second day

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const App=()=>{
+   return(
+    <div>
+    <h1>Hello Cybrom</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, quos.</p>
+    </div>
+  
+   )    
+}
+export default App
