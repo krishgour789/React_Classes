6f79ec1e065a5868b80e434d76de06260c1a2108
krishgour789 <krishgour789@gmail.com>
2025-11-26 17:33:29 +0530

    Tailwind Class

--- src/Tailwind.jsx
+++ src/Tailwind.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import './Tailwind.css'
+import { FaHome } from "react-icons/fa";
+import { IoIosContact } from "react-icons/io";
+import { FaHandsHelping } from "react-icons/fa";
+
 
 const App = () => {
     return (
         <>
-        <nav className="p-4 border-b">
+        <nav className="p-4 border-b items-center">
       <div className="flex justify-between items-center ">
-        <h1 className="font-bold">App</h1>
+        <img src="vite.svg" alt="" />
        
-        <ul className="flex gap-3.5">
-            <li>Home</li>
-            <li>Contact</li>
-            <li>Help</li>
+
+        <ul className="hidden sm:flex items-center gap-5 ">
+            <li className="flex items-center"><FaHome />  Home</li>
+            <li className="flex items-center"><IoIosContact /> Contact</li>
+            <li className="flex items-center"><FaHandsHelping /> Help</li>
         </ul>
+        {/* // Media Query
+        0-640 px = By Default
+        640-768 px = SM:
+        768-1024 px = MD:
+        1024+ = Lg: */}
       </div>
     </nav>
+    <div className="text-amber-700 text-3xl">
+      <h1 className="text-cyan-800">Lorem, ipsum dolor.</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nobis architecto odit tempore hic modi eaque deleniti quae itaque aspernatur.</p>
+    </div>
         </>
     )
 }
